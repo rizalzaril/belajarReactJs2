024bbd95752ad86ba0729b990a4f36fbe1189416
Rizal <zaril.ziral2020@gmail.com>
2024-09-12 19:16:04 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Rizal <code>src/App.js</code> LIYAN SYAH.
         </p>
         <a
           className="App-link"
